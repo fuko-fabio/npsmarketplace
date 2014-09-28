797810c6e99ebe2ddd200d8486bbee91891683ae
root <root@vz25450.(none)>
2014-09-28 16:16:28 -0400

    Translations

---
+++ modules/npsprzelewy24/translations/pl.php
@@ -0,0 +1,21 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{npsprzelewy24}prestashop>payment_register_company_25f7ba9fc064d9eca1b0e3528cc4699c'] = 'Konfiguracja płatności';
+$_MODULE['<{npsprzelewy24}prestashop>payment_register_company_d5815623c49bc79327917762848902f0'] = 'Ustawienia płatności';
+$_MODULE['<{npsprzelewy24}prestashop>payment_register_company_e7b47c58815acf1d3afa59a84b5db7fb'] = 'Nazwa firmy';
+$_MODULE['<{npsprzelewy24}prestashop>payment_register_company_40bed7cf9b3d4bb3a3d7a7e3eb18c5eb'] = 'Imię i Nazwisko';
+$_MODULE['<{npsprzelewy24}prestashop>payment_register_company_ce8ae9da5b7cd6c3df2929543a9af92d'] = 'Adres email';
+$_MODULE['<{npsprzelewy24}prestashop>payment_register_company_dd7bf230fde8d4836917806aff6a6b27'] = 'Adres';
+$_MODULE['<{npsprzelewy24}prestashop>payment_register_company_fb85dee77167699087f1d6eec1d2fbb7'] = 'Kod pocztowy';
+$_MODULE['<{npsprzelewy24}prestashop>payment_register_company_57d056ed0984166336b7879c2af3657f'] = 'Miasto';
+$_MODULE['<{npsprzelewy24}prestashop>payment_register_company_2be69d2c4f6a31bb2ae7db875035037e'] = 'NIP';
+$_MODULE['<{npsprzelewy24}prestashop>payment_register_company_35dc9ae0735af64abf9438b80e6d6298'] = 'REGON';
+$_MODULE['<{npsprzelewy24}prestashop>payment_register_company_d26c078fbaa4300428fc1de134660f0b'] = 'Numer konta';
+$_MODULE['<{npsprzelewy24}prestashop>payment_register_company_d872567abb1da7730709a9d1136d67d9'] = 'W formacie IBAN. Pzykład: PL 74 3535 1100...';
+$_MODULE['<{npsprzelewy24}prestashop>payment_register_company_2e16f351fe2e610c9364c4aa7c74dcf8'] = 'Akceptuję';
+$_MODULE['<{npsprzelewy24}prestashop>payment_register_company_cd01fbfbb767c30ef555acaf27f5c71a'] = 'regulamin Przelewy24';
+$_MODULE['<{npsprzelewy24}prestashop>payment_register_company_0ba7583639a274c434bbe6ef797115a4'] = 'Wyślij';
+$_MODULE['<{npsprzelewy24}prestashop>payment_return_fb077ecba55e5552916bde26d8b9e794'] = 'Potwierdzenie zamówienia';
+$_MODULE['<{npsprzelewy24}prestashop>payment_c8ed360cbe1ba919f8c40f07a31125d7'] = 'Zapłać z PRzelewy24';
---
+++ themes/minimal_16_theme/lang/pl.php
@@ -0,0 +1,14 @@
+<?php
+
+global $_LANG;
+$_LANG = array();
+$_LANG['404_44b1d05764b093cb00c25b8228cb87d3'] = 'Ta strona jest niedostępna';
+$_LANG['404_6554aae881e40036cac1f5710a4c52c1'] = 'Przepraszamy ale podany adres strony internetowej nie jest dostępny.';
+$_LANG['404_7b19b37e7fc3dfe3c3e1ad5b84c7f565'] = 'Strona główna';
+$_LANG['404_8cf04a9734132302f96da8e113e80ce5'] = 'Strona domowa';
+$_LANG['404_9bec38acab82302a8cf6fbb9182da21f'] = 'Aby znaleźć produkt proszę podać jego nazwę w polu poniżej.';
+$_LANG['404_a60852f204ed8028c1c58808b746d115'] = 'OK';
+$_LANG['404_a68ba7d969d1294a35138fa501dfb078'] = 'Przeszukaj katalog wydarzeń: ';
+$_LANG['404_d0fbda9855d118740f1105334305c126'] = 'NIe odnaleziono strony';
+
+?>
