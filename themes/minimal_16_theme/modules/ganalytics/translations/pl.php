4e891c5b032f6d01c14cfcc20454ddb7515054e4
root <root@labsintown.pl>
2015-03-23 21:53:34 +0100

    Theme translations

---
+++ themes/minimal_16_theme/modules/ganalytics/translations/pl.php
@@ -0,0 +1,8 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{ganalytics}minimal_16_theme>ganalytics_c9cc8cce247e49bae79f15173ce97354'] = 'Zapisz';
+$_MODULE['<{ganalytics}minimal_16_theme>ganalytics_630f6dc397fe74e52d5189e2c80f282b'] = 'Powrót do listy';
+$_MODULE['<{ganalytics}minimal_16_theme>ganalytics_462390017ab0938911d2d4e964c0cab7'] = 'Pomyślnie zaktualizowano ustawienia';
+$_MODULE['<{ganalytics}minimal_16_theme>form-ps14_c9cc8cce247e49bae79f15173ce97354'] = 'Zapisz';
---
+++ themes/minimal_16_theme/modules/gapi/translations/pl.php
@@ -0,0 +1,29 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{gapi}minimal_16_theme>gapi_69ee9bf9cf3d83a8468278c44959caf0'] = 'Google Analytics API';
+$_MODULE['<{gapi}minimal_16_theme>gapi_0851f7a0844553fa1168718de0f87262'] = 'Nie wolno otwierać zewnętrznych adresów URL';
+$_MODULE['<{gapi}minimal_16_theme>gapi_6401593f1412a6b385c8e645d1f056ac'] = 'cURL nie jest włączony';
+$_MODULE['<{gapi}minimal_16_theme>gapi_f8b94463fa8b5591e5edbbb8021e8038'] = 'OpenSSL nie jest włączona';
+$_MODULE['<{gapi}minimal_16_theme>gapi_6e4c3e76dd29876e6d33ce8c89e5fc5f'] = 'Google jest niedostępne (sprawdź swój firewall)';
+$_MODULE['<{gapi}minimal_16_theme>gapi_a1ed99ed6aaac91d7c3b127f032abf2d'] = 'Obecnie testujesz swój sklep na serwerze lokalnym. Aby cieszyć się wszystkimi funkcjami, musisz przenieść swój sklep na serwer online.';
+$_MODULE['<{gapi}minimal_16_theme>gapi_2ccf68a6aec8eda73156a7ef54b03351'] = 'Którą wersję Google Analytics API chcesz użyć?';
+$_MODULE['<{gapi}minimal_16_theme>gapi_0caf30452ef28d761ae80a407b64bd9b'] = 'v1.3: łatwa w konfiguracji, ale nie rozwijana i mniej bezpieczna';
+$_MODULE['<{gapi}minimal_16_theme>gapi_949617ff3314c7cf2d88d356a953bd67'] = 'v3.0 z OAuth 2.0: najmocniejsza i aktualna wersja';
+$_MODULE['<{gapi}minimal_16_theme>gapi_f1f4f41c5cab767032db832ec7bd5b64'] = 'Zapisz i skonfiguruj';
+$_MODULE['<{gapi}minimal_16_theme>gapi_00e9b476102174b72bce85f57ef4f251'] = 'Wczoraj, Twój sklep otrzymał wizytę %d osób w sumie %d unikalnych odsłon.';
+$_MODULE['<{gapi}minimal_16_theme>gapi_6489ed26701b74c0fb139a3368804121'] = 'Powinieneś teraz mieć coś takiego';
+$_MODULE['<{gapi}minimal_16_theme>gapi_6e1e99918b40cf3f46166fae1e642b73'] = 'Powinieneś mieć teraz następujący ekran. Kopiuj/Wklej informacje \"Client ID\" oraz \"Client secret\" do poniższego formularza';
+$_MODULE['<{gapi}minimal_16_theme>gapi_b18cb8e83113953f96bbe47bd90ab69c'] = 'Google Analytics API v3.0';
+$_MODULE['<{gapi}minimal_16_theme>gapi_76525f0f34b48475e5ca33f71d296f3b'] = 'ID klienta';
+$_MODULE['<{gapi}minimal_16_theme>gapi_734082edf44417dd19cc65943aa65c36'] = 'Hasło klienta';
+$_MODULE['<{gapi}minimal_16_theme>gapi_cce99c598cfdb9773ab041d54c3d973a'] = 'Profil';
+$_MODULE['<{gapi}minimal_16_theme>gapi_b1a026d322c634ca9e88525070e012fd'] = 'Zapisz i uwierzytelnij';
+$_MODULE['<{gapi}minimal_16_theme>gapi_d4e6d6c42bf3eb807b8778255a4ce415'] = 'Autoryzacja nie powiodła się';
+$_MODULE['<{gapi}minimal_16_theme>gapi_a670b4cdb42644e4b46fa857d3f73d9e'] = 'Google Analytics API v1.3';
+$_MODULE['<{gapi}minimal_16_theme>gapi_ce8ae9da5b7cd6c3df2929543a9af92d'] = 'E-mail';
+$_MODULE['<{gapi}minimal_16_theme>gapi_dc647eb65e6711e155375218212b3964'] = 'Hasło';
+$_MODULE['<{gapi}minimal_16_theme>gapi_970a710b7344f8639b6a86d1f081b660'] = 'Można znaleźć teraz ID w pasku adresu przeglądarki podczas korzystania z raportu Analytics.';
+$_MODULE['<{gapi}minimal_16_theme>gapi_e33d3b3409f8a0fcc326596c918c4961'] = 'Dla STARSZEJ WERSJI Google Analytics, ID profilu znajduje się po \"id\" w adresie URL (szukaj \"&id=xxxxxxxx\"):';
+$_MODULE['<{gapi}minimal_16_theme>gapi_c78fedea48082c7a437773e31b418f96'] = 'W NOWSZEJ WERSJI Google Analytics, ID profilu to numer, który występuje po znaku p:';
