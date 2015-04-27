0f458f539402339f6a4a9e192107c7e44ec6833c
root <root@labsintown.pl>
2015-04-28 01:30:17 +0200

    Translations

---
+++ themes/minimal_16_theme/modules/npscombinations/translations/pl.php
@@ -0,0 +1,15 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{npscombinations}minimal_16_theme>tab_content_a1fa27779242b4902f7ae3bdd5c6d508'] = 'Typ';
+$_MODULE['<{npscombinations}minimal_16_theme>tab_content_49ee3087348e8d44e1feda1917443987'] = 'Rodzaj';
+$_MODULE['<{npscombinations}minimal_16_theme>tab_content_359ff4ae83f885f7d3227a4353ea525e'] = 'Data wydarzenia';
+$_MODULE['<{npscombinations}minimal_16_theme>tab_content_78945de8de090e90045d299651a68a9b'] = 'Dostępny do';
+$_MODULE['<{npscombinations}minimal_16_theme>tab_content_3601146c4e948c32b6424d2c0a7f0118'] = 'Cena';
+$_MODULE['<{npscombinations}minimal_16_theme>tab_content_694e8d1f2ee056f98ee488bdc4982d73'] = 'Ilość';
+$_MODULE['<{npscombinations}minimal_16_theme>tab_content_c751439d0db3883ac1c8e816327adcab'] = 'Bilet';
+$_MODULE['<{npscombinations}minimal_16_theme>tab_content_05a79b3e220b02798e20b0f4e1bd1431'] = 'Karnet';
+$_MODULE['<{npscombinations}minimal_16_theme>tab_content_0e39aaa8762149a023239fb4a3705946'] = 'Wyprzedane!';
+$_MODULE['<{npscombinations}minimal_16_theme>tab_content_2d0f6b8300be19cf35e89e66f0677f95'] = 'Do koszyka';
+$_MODULE['<{npscombinations}minimal_16_theme>tab_b9208b03bcc9eb4a336258dcdcb66207'] = 'Bilety';
